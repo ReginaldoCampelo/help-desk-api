cd16f1908cb5a8d6a12009aafadf067a16fabed6
Reginaldo Campelo <regin.developer@gmail.com>
2022-03-14 10:07:50 -0300

    adding validation for fields form

--- src/main/java/com/reginaldo/helpdesk/domain/dtos/TecnicoDTO.java
+++ src/main/java/com/reginaldo/helpdesk/domain/dtos/TecnicoDTO.java
@@ -6,6 +6,8 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import javax.validation.constraints.NotNull;
+
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.reginaldo.helpdesk.domain.Tecnico;
 import com.reginaldo.helpdesk.domain.enums.Perfil;
@@ -14,9 +16,17 @@ public class TecnicoDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	protected Integer id;
+	
+	@NotNull(message = "O campo NOME é obrigatório!")
 	protected String nome;
+	
+	@NotNull(message = "O campo CPF é obrigatório!")
 	protected String cpf;
+	
+	@NotNull(message = "O campo EMAIL é obrigatório!")
 	protected String email;
+	
+	@NotNull(message = "O campo SENHA é obrigatório!")
 	protected String senha;
 	protected Set<Integer> perfis = new HashSet<>();
 	
--- src/main/java/com/reginaldo/helpdesk/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/reginaldo/helpdesk/resources/exceptions/ResourceExceptionHandler.java
@@ -4,6 +4,8 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -32,6 +34,21 @@ public class ResourceExceptionHandler {
 				"Data Violation", ex.getMessage(), request.getRequestURI());
 		
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+		
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StandardError> validationErrors(MethodArgumentNotValidException ex,
+			HttpServletRequest request) {
+		
+		ValidationError errors = new ValidationError(System.currentTimeMillis(),
+				HttpStatus.BAD_REQUEST.value(), "Validation error", "Erro na validação dos campos", request.getRequestURI());
+		
+		for(FieldError x : ex.getBindingResult().getFieldErrors()) {
+			errors.addErrors(x.getField(), x.getDefaultMessage());
+		}
+		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
 		
 	}
 
