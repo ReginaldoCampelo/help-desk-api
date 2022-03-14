683d242f924f7234ab5b0b76baec2604aac4abba
Reginaldo Campelo <regin.developer@gmail.com>
2022-03-14 14:24:51 -0300

    adding findById for chamado

---
+++ src/main/java/com/reginaldo/helpdesk/resources/ChamadoResource.java
@@ -0,0 +1,27 @@
+package com.reginaldo.helpdesk.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.reginaldo.helpdesk.domain.Chamado;
+import com.reginaldo.helpdesk.domain.dtos.ChamadoDTO;
+import com.reginaldo.helpdesk.services.ChamadoService;
+
+@RestController
+@RequestMapping(value = "/chamados")
+public class ChamadoResource {
+	
+	@Autowired
+	private ChamadoService service;
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<ChamadoDTO> findById(@PathVariable Integer id) {
+		Chamado obj = service.findById(id);
+		return ResponseEntity.ok().body(new ChamadoDTO(obj));
+	}
+
+}
---
+++ src/main/java/com/reginaldo/helpdesk/services/ChamadoService.java
@@ -0,0 +1,22 @@
+package com.reginaldo.helpdesk.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.reginaldo.helpdesk.domain.Chamado;
+import com.reginaldo.helpdesk.repositories.ChamadoRepository;
+import com.reginaldo.helpdesk.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ChamadoService {
+	
+	@Autowired
+	private ChamadoRepository repository;
+
+	public Chamado findById(Integer id) {
+		Optional<Chamado> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! ID: " + id));
+	}
+}
