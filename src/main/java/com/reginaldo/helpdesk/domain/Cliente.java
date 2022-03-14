96e7553155ff0dc79d6db2d5a14bb81e29546509
Reginaldo Campelo <regin.developer@gmail.com>
2022-03-14 11:29:02 -0300

    implementing crud for clientes

--- src/main/java/com/reginaldo/helpdesk/domain/Cliente.java
+++ src/main/java/com/reginaldo/helpdesk/domain/Cliente.java
@@ -2,11 +2,13 @@ package com.reginaldo.helpdesk.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.reginaldo.helpdesk.domain.dtos.ClienteDTO;
 import com.reginaldo.helpdesk.domain.enums.Perfil;
 
 @Entity
@@ -25,6 +27,17 @@ public class Cliente extends Pessoa {
 	public Cliente(Integer id, String nome, String cpf, String email, String senha) {
 		super(id, nome, cpf, email, senha);
 		addPerfil(Perfil.CLIENTE);
+	}
+	
+	public Cliente(ClienteDTO obj) {
+		super();
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.cpf = obj.getCpf();
+		this.email = obj.getEmail();
+		this.senha = obj.getSenha();
+		this.perfis = obj.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+		this.dataCriacao = obj.getDataCriacao();
 	}
 
 	public List<Chamado> getChamados() {
---
+++ src/main/java/com/reginaldo/helpdesk/services/ClienteService.java
@@ -0,0 +1,75 @@
+package com.reginaldo.helpdesk.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.reginaldo.helpdesk.domain.Cliente;
+import com.reginaldo.helpdesk.domain.Pessoa;
+import com.reginaldo.helpdesk.domain.dtos.ClienteDTO;
+import com.reginaldo.helpdesk.repositories.ClienteRepository;
+import com.reginaldo.helpdesk.repositories.PessoaRepository;
+import com.reginaldo.helpdesk.services.exceptions.DataIntegrityViolationException;
+import com.reginaldo.helpdesk.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repository;
+	
+	@Autowired
+	private PessoaRepository pessoaRepository;
+	
+	public Cliente findById(Integer id) {
+		Optional<Cliente> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id));
+	}
+
+	public List<Cliente> findAll() {
+		return repository.findAll();
+	}
+
+	public Cliente create(ClienteDTO objDTO) {
+		objDTO.setId(null);
+		validaPorCpfEEmail(objDTO);
+		Cliente newObj = new Cliente(objDTO);
+		return repository.save(newObj);
+	}
+	
+	public Cliente update(Integer id, @Valid ClienteDTO objDTO) {
+		objDTO.setId(id);
+		Cliente oldObj = findById(id);
+		validaPorCpfEEmail(objDTO);
+		oldObj = new Cliente(objDTO);
+		return repository.save(oldObj);
+		
+	}
+	
+	public void delete(Integer id) {
+		Cliente obj = findById(id);
+		if(obj.getChamados().size() > 0) {
+			throw new DataIntegrityViolationException("Cliente possui ordens de serviço e não pode ser deletado!");
+		}
+		repository.deleteById(id);
+		
+	}
+
+
+	private void validaPorCpfEEmail(ClienteDTO objDTO) {
+		Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
+		if(obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+			throw new DataIntegrityViolationException("CPF já cadastrado no sistema!");
+		}
+		
+		obj = pessoaRepository.findByEmail(objDTO.getEmail());
+		if(obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+			throw new DataIntegrityViolationException("E-mail já cadastrado no sistema!");
+		}
+		
+	}
+}
