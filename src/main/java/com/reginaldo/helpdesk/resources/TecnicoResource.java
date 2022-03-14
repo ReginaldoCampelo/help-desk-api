4cca161a1b42e562d933970de22d02d617a98692
Reginaldo Campelo <regin.developer@gmail.com>
2022-03-13 22:27:01 -0300

    adding end point findAll for tecnicos

--- src/main/java/com/reginaldo/helpdesk/resources/TecnicoResource.java
+++ src/main/java/com/reginaldo/helpdesk/resources/TecnicoResource.java
@@ -1,5 +1,8 @@
 package com.reginaldo.helpdesk.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,6 +25,14 @@ public class TecnicoResource {
 	public ResponseEntity<TecnicoDTO> findById(@PathVariable Integer id) {
 		Tecnico obj = service.findById(id);
 		return ResponseEntity.ok().body(new TecnicoDTO(obj));
+		
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<TecnicoDTO>> findAll() {
+		List<Tecnico> list = service.findAll();
+		List<TecnicoDTO> listDTO = list.stream().map(obj -> new TecnicoDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
 		
 	}
 
--- src/main/java/com/reginaldo/helpdesk/services/TecnicoService.java
+++ src/main/java/com/reginaldo/helpdesk/services/TecnicoService.java
@@ -1,5 +1,6 @@
 package com.reginaldo.helpdesk.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +19,10 @@ public class TecnicoService {
 	public Tecnico findById(Integer id) {
 		Optional<Tecnico> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id));
+	}
+
+	public List<Tecnico> findAll() {
+		return repository.findAll();
 	}
 
 }
