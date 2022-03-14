93138e6ecf825f54fc481de614c0ee2e52e8e63f
Reginaldo Campelo <regin.developer@gmail.com>
2022-03-13 22:54:00 -0300

    adding end point create for tecnico

--- src/main/java/com/reginaldo/helpdesk/services/TecnicoService.java
+++ src/main/java/com/reginaldo/helpdesk/services/TecnicoService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.reginaldo.helpdesk.domain.Tecnico;
+import com.reginaldo.helpdesk.domain.dtos.TecnicoDTO;
 import com.reginaldo.helpdesk.repositories.TecnicoRepository;
 import com.reginaldo.helpdesk.services.exceptions.ObjectNotFoundException;
 
@@ -23,6 +24,12 @@ public class TecnicoService {
 
 	public List<Tecnico> findAll() {
 		return repository.findAll();
+	}
+
+	public Tecnico create(TecnicoDTO objDTO) {
+		objDTO.setId(null);
+		Tecnico newObj = new Tecnico(objDTO);
+		return repository.save(newObj);
 	}
 
 }
